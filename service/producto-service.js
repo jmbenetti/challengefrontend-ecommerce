0ad966d1fc6364096d95b53b28f157d809ae8305
José Manuel Benetti <josemanuelbenetti@gmail.com>
2022-05-09 21:07:03 -0300

    bugfix

--- service/producto-service.js
+++ service/producto-service.js
@@ -1,30 +1,30 @@
-// const servidorJSON = "https://heroku-json-jmb.herokuapp.com/productos";
-const servidorJSON = "http://localhost:3000/productos";
+const servidorJSON = "https://heroku-json-jmb.herokuapp.com/productos";
+// const servidorJSON = "http://localhost:3000/productos";
 
-// const listaProductos = () => fetch(servidorJSON).then(respuesta => respuesta.json());
+const listaProductos = () => fetch(servidorJSON).then(respuesta => respuesta.json());
 
-// const crearProducto = (nombre, precio, categoria, imagen) => {
-//     return fetch(servidorJSON, 
-//     {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json"
-//         }, 
-//         body: JSON.stringify({nombre, precio, categoria, imagen, id: uuid.v4()}),
+const crearProducto = (nombre, precio, categoria, imagen) => {
+    return fetch(servidorJSON, 
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }, 
+        body: JSON.stringify({nombre, precio, categoria, imagen, id: uuid.v4()}),
 
-//     })
-// }
+    })
+}
 
-// const eliminarProducto = (id) => {
-//     //return fetch(`http://localhost:3000/productos/${id}`,
-//     return fetch(servidorJSON + id,
-//     {
-//         method: "DELETE",
-//     })
-// }
+const eliminarProducto = (id) => {
+    //return fetch(`http://localhost:3000/productos/${id}`,
+    return fetch(servidorJSON + id,
+    {
+        method: "DELETE",
+    })
+}
 
-// export const productServices = {
-//     listaProductos,
-//     crearProducto,
-//     eliminarProducto,
-// }
+export const productServices = {
+    listaProductos,
+    crearProducto,
+    eliminarProducto,
+}
